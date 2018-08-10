25709dfdeec1cb66d864f0e6ac97b10b8220ede4
maclaptop <do189705@gmail.com>
2018-08-10 09:43:08 -0700

    user state updates navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,23 +1,23 @@
 <template lang="html">
   <nav class="navbar" role="navigation" aria-label="main navigation">
     <div class="navbar-brand">
-      <a class="navbar-item" href="https://bulma.io">
-        <h1 class="is-size-3">ListWar</h1>
-      </a>
-
-      <a class="navbar-burger" role="button" aria-label="menu" aria-expanded="false">
+      <h1 class="is-size-3">ListWar</h1>
+      <a @click="openMenu = !openMenu" :class="{ 'is-active': openMenu } " class="navbar-burger" role="button" aria-label="menu" aria-expanded="false">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
-    <div class="navbar-menu">
+    <div class="navbar-menu" :class="{ 'is-active': openMenu }">
       <div class="navbar-end">
         <div class="navbar-item">
-          <router-link :to="{ name: 'SignIn', params: {} }">sign-in</router-link>
+          <router-link v-show="!user" :to="{ name: 'SignIn' }">Sign In</router-link>
         </div>
         <div class="navbar-item">
-          <router-link :to="{ name: 'SignOut', params: {} }">sign-out</router-link>
+          <router-link v-show="!user" :to="{ name: 'SignOut' }">Sign Out</router-link>
+        </div>
+        <div class="navbar-item">
+          <router-link v-show="user" :to="{ name: '/' }">Log Out</router-link>
         </div>
       </div>
     </div>
@@ -26,6 +26,16 @@
 
 <script>
 export default {
+  data () {
+    return {
+      openMenu: false
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.getters.getUser
+    }
+  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import SignIn from '@/components/SignIn'
+import SignOut from '@/components/SignOut'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/',
+      name: 'SignIn',
+      component: SignIn
+    },
+    {
+      path: '/',
+      name: 'SignOut',
+      component: SignOut
     }
   ]
 })
